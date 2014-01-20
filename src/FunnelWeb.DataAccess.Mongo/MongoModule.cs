2bd472fdddd778b7e12602ad411911fec0d4aa89
Matthew Nichols <matthew@matthew-nichols.com>
2014-01-20 08:19:58 -0700

    Started a Mongo Persistence Layer

---
+++ src/FunnelWeb.DataAccess.Mongo/MongoModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace FunnelWeb.DataAccess.Mongo
+{
+    public class MongoModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<AdminRepository>().As<IAdminRepository>().InstancePerLifetimeScope();
+            //builder.RegisterType<TaskStateRepository>().As<ITaskStateRepository>().InstancePerLifetimeScope();
+
+            //builder.RegisterType<NHibernateRepository>().As<IRepository>().InstancePerLifetimeScope();
+
+            //builder.Register(ConfigureSessionFactory).As<ISessionFactory>().SingleInstance();
+            //builder.Register(c => c.Resolve<ISessionFactory>().OpenSession()).As<ISession>().InstancePerLifetimeScope();
+        }
+
+    }
+}
---
+++ src/FunnelWeb.DataAccess.Mongo/Providers/MongoDbProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunnelWeb.Domain.Interfaces;
+
+namespace FunnelWeb.DataAccess.Mongo.Providers
+{
+    public class MongoDbProvider : IDatabaseProvider
+    {
+        public string DefaultConnectionString { get; private set; }
+        public bool SupportSchema { get; private set; }
+        public bool SupportFuture { get; private set; }
+        public bool SupportsFullText { get; private set; }
+        public bool TryConnect(string connectionString, out string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPersistenceConfigurer GetDatabaseConfiguration(IConnectionStringSettings connectionStringSettings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Func<IDbConnection> GetConnectionFactory(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UpgradeEngineBuilder GetUpgradeEngineBuilder(string connectionString, string schema)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
